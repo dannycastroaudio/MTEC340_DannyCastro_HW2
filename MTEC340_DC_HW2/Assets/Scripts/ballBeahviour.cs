a72d003c9ec154bc018a0611c002ede627fa364d
dannycastroaudio <dannycastroaudio@gmail.com>
2026-02-04 02:04:51 -0500

    added scripts and comments. it works yay

--- MTEC340_DC_HW2/Assets/Scripts/ballBeahviour.cs
+++ MTEC340_DC_HW2/Assets/Scripts/ballBeahviour.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class ballBeahviour : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float Speed = 3.0f;
+    private Vector2 direction;
+    private void Start()
     {
-        
-    }
+        direction.x = Random.value > 0.5f ? 1 : -1;
+        direction.y = Random.value > 0.5f ? 1 : -1;
 
-    // Update is called once per frame
+    }
     void Update()
     {
-        
+        Vector3 movement = direction * Speed * Time.deltaTime;
+        transform.Translate(movement);
     }
 }
--- MTEC340_DC_HW2/Assets/Scripts/paddleBehaviour.cs
+++ MTEC340_DC_HW2/Assets/Scripts/paddleBehaviour.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class paddleBehaviour : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float Speed;
+    public float LimitLeft = -9.2f; //I guessed these values. could be cleaner. ended up being smaller than 9.2
+    public float LimitRight = 9.2f;
+    public KeyCode Left = KeyCode.A;
+    public KeyCode Right = KeyCode.D;
 
-    // Update is called once per frame
     void Update()
     {
-        
-    }
+        float movement = 0.0f;
+        if (Input.GetKey(Left))
+        {
+            movement -= Speed;
+        }
+
+        if (Input.GetKey(Right))
+        {
+            movement += Speed;
+        }
+        //the variable movement is affecting the left axis
+        transform.Translate(new Vector3(movement, 0.0f, 0.0f)*Time.deltaTime); 
+        // clampedX establishes a perimeter in the x-based on the LimitLeft and LimitRight floats
+        //its restricting the x axis basically
+        float clampedX = Mathf.Clamp(transform.position.x , LimitLeft, LimitRight );
+        Vector3 pos = transform.position; //ref to paddle's position
+        pos.x = clampedX; //the x position is limited by clampedX
+        transform.position = pos; //the position of the paddle is limited within the clampedX equation
+
 }
